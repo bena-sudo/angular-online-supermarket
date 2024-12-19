807bd935914413f431c812ede910ff63f4d9cbe5
bena-sudo <joanbenaventcastells@gmail.com>
2024-12-19 23:18:47 +0100

    Online supermarket project start.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainComponent }, // Ruta raíz
+    { path: 'login', component: LoginComponent }, // Ruta /about
+    { path: '**', redirectTo: '' }, 
+  ];
