d05bf8dfa880c989a82adb91c9f4c76df78f23ed
bena-sudo <joanbenaventcastells@gmail.com>
2024-12-22 23:24:42 +0100

    Format Document

--- src/app/features/products/service/product.service.ts
+++ src/app/features/products/service/product.service.ts
@@ -6,7 +6,7 @@ import { Product } from '../../../core/models/product.model';
   providedIn: 'root',
 })
 export class ProductService {
-  private readonly table = 'product';
+  private readonly table: string = 'product';
 
   constructor(private readonly supabase: SupabaseService) {}
 
@@ -14,42 +14,66 @@ export class ProductService {
     const { data, error } = await this.supabase.client
       .from(this.table)
       .select('*');
-    if (error) throw new Error(error.message);
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
     return data || [];
   }
-  async getProductById(id: string): Promise<any> {
+
+  async getProductById(id: string): Promise<Product | null> {
     const { data, error } = await this.supabase.client
       .from(this.table)
       .select('*')
       .eq('id', id)
       .single();
-    if (error) throw new Error(error.message);
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
     return data;
   }
 
-  async createProduct(product: any): Promise<any> {
+  async createProduct(product: Product): Promise<Product[] | null> {
     const { data, error } = await this.supabase.client
       .from(this.table)
       .insert([product]);
-    if (error) throw new Error(error.message);
-    return data;
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return data || null;
   }
 
-  async updateProduct(id: string, updates: any): Promise<any> {
+  async updateProduct(
+    id: string,
+    updates: Partial<Product>
+  ): Promise<Product[] | null> {
     const { data, error } = await this.supabase.client
       .from(this.table)
       .update(updates)
       .eq('id', id);
-    if (error) throw new Error(error.message);
-    return data;
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return data || null;
   }
 
-  async deleteProduct(id: string): Promise<any> {
+  async deleteProduct(id: string): Promise<Product[] | null> {
     const { data, error } = await this.supabase.client
       .from(this.table)
       .delete()
       .eq('id', id);
-    if (error) throw new Error(error.message);
-    return data;
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return data || null;
   }
 }
