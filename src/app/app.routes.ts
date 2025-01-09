3aa7766b640aae0638584bc9340f66f78da93e09
bena-sudo <joanbenaventcastells@gmail.com>
2025-01-09 11:59:09 +0100

    Add product listing page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './features/auth/pages/login/login.component';
 import { ProductMainComponent } from './features/products/pages/product-main/product-main.component';
 import { ProductDetailComponent } from './features/products/pages/product-detail/product-detail.component';
 import { CategoryMainComponent } from './features/categories/pages/category-main/category-main.component';
+import { ProductListComponent } from './features/products/pages/product-list/product-list.component';
 
 export const routes: Routes = [
   { path: '', component: ProductMainComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'products', component: ProductListComponent },
   { path: 'product/:id', component: ProductDetailComponent },
   { path: 'categories', component: CategoryMainComponent },
   { path: 'categories/:id', component: CategoryMainComponent },
---
+++ src/app/features/products/pages/product-list/product-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FooterComponent } from "../../../../shared/components/footer/footer.component";
+import { ProductGridComponent } from "../../components/product-grid/product-grid.component";
+import { HeaderComponent } from "../../../../shared/components/header/header.component";
+import { Product } from '../../../../core/models/product.model';
+import { ProductService } from '../../service/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [FooterComponent, ProductGridComponent, HeaderComponent],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private readonly productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  async loadProducts(): Promise<void> {
+    try {
+      this.products = await this.productService.getProducts();
+    } catch (error) {
+      console.error('Error when loading products:', error);
+    }
+  }
+}
